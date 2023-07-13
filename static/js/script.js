574cd017f18c3d6fe9a4d19f2319dfd378aca7af
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-13 13:03:41 +0300

    addIncome func.. selecting html elements then check condi..

--- static/js/script.js
+++ static/js/script.js
@@ -6,5 +6,13 @@ let expenseData = [];
 function addIncome(event) {
   event.preventDefault();
   
+  const description = document.querySelector('#income-description').value;
+  const amount = parseFloat(document.querySelector('#income-amount').value);
 
+  if (description && amount) {
+    incomeData.push({ description, amount });
+    updateBudget();
+    updateIncomeList();
+    clearInputFields('#income-form');
+  }
 }
